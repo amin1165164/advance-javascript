3e29d84340e9571a72c74bfe7be88a07dac5a9b8
amin1165164 <aminul20403@gmail.com>
2021-02-05 01:04:48 +0600

    advance javascript

---
+++ object-property.js
@@ -0,0 +1,12 @@
+const students = [
+    {id: 1, name : 'yeamin'},
+    {id: 2, name : 'emon'},
+    {id: 3, name : 'arafath'}
+]
+const students1 = [];
+for(let i = 0; i < students.length; i++){
+    const x = students[i];
+    students1.push(x);
+    
+}
+console.log(students1);
